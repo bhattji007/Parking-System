567ef71ab36bbc8d779587a1f0bb7a8f508712fa
Shubham Bhatt <shubhambhattrocks123@gmail.com>
2022-12-27 19:18:44 +0530

    new

--- frontend/src/requests.js
+++ frontend/src/requests.js
@@ -2,6 +2,6 @@ const Base="http://localhost:5000";
 
 const requests={
     park_in:`${Base}/vehicle/in`,
-    park_out:`${Base}/vehicle/in`
+    park_out:`${Base}/vehicle/out`
 }
  export default requests;
